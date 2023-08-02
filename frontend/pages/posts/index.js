3d81874423dc692fe96ee59563fd45c8bdf772a3
Elham Alizadeh <92359726+elhamalizadeh@users.noreply.github.com>
2023-08-02 22:38:43 +0330

    show posts

--- frontend/pages/posts/index.js
+++ frontend/pages/posts/index.js
@@ -1,7 +1,63 @@
-const Posts = () =>{
-    return(
-        <h1>Posts</h1>
-    )
+import { toast } from "react-toastify";
+import { useEffect } from "react";
 
+const Posts = ({ posts, error }) => {
+    useEffect(() => {
+        error && toast.error(error)
+    }, [error]);
+
+    return (
+        <div className="container mt-5">
+            <div className="row g-3">
+                {posts && posts.map(post => (
+                    <div key={post.id} className="col-md-12">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{post.title}</h5>
+                                <p className="card-text">{post.body}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
+
 export default Posts;
+
+export async function getServerSideProps({ req }) {
+    try {
+        const resApi = await fetch('http://localhost:8000/api/posts', {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${req.cookies.token}`
+            }
+        });
+        const data = await resApi.json();
+
+        if (resApi.ok) {
+            return {
+                props: {
+                    posts: data.posts,
+                    error: null
+                }
+            }
+        } else {
+            return {
+                props: {
+                    posts: null,
+                    error: data
+                }
+            }
+        }
+
+    } catch (e) {
+        return {
+            props: {
+                posts: null,
+                error: 'Server Error'
+            }
+        }
+    }
+}
